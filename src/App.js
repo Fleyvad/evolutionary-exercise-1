1ca7d8e471b388bd594bfbfbb9c78667b06d2e4c
Paco Leyva <118618592+Fleyvad@users.noreply.github.com>
2023-05-15 13:45:31 +0200

    create clock component and styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Clock from "./components/pure/Clock";
 import ComponentA from "./components/pure/ComponentA";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Contact</h1>
         <ComponentA></ComponentA>
+        <Clock></Clock>
       </header>
     </div>
   );
